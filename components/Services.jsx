7c0f5597b96e421aac7b695d41916d6a2306d685
khannsameer <sk8717108@gmail.com>
2025-09-13 21:16:56 +0530

    Update Services section and global styles
    - Updated Services.jsx with improvements
    - Updated globals.css for styling changes

--- components/Services.jsx
+++ components/Services.jsx
@@ -15,18 +15,26 @@ const Services = () => {
         seamless digital experiences tailored to your needs.
       </p>
 
-      <div className="grid grid-cols-4 gap-6 my-10">
+      <ul className="grid grid-auto gap-6 my-10">
         {serviceData.map(({ icon, title, description, link }, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-12 dark:border-gray-700 cursor-pointer transition duration-300 transform hover:-translate-y-2 hover:shadow-xl dark:hover:shadow-2xl hover:bg-gray-100 dark:hover:bg-gray-800"
+          >
             <Image src={icon} alt="" className="w-10" />
-            <h3 className="text-lg my-4 text-gray-700">{title}</h3>
-            <p className="text-sm text-gray-600 leading-5">{description}</p>
+            <h3 className="text-lg my-4 text-gray-800 dark:text-gray-200 group-hover:text-gray-900 dark:group-hover:text-white">
+              {title}
+            </h3>
+            <p className="text-sm leading-5 text-gray-800 dark:text-gray-200 group-hover:text-gray-900 dark:group-hover:text-white">
+              {description}
+            </p>
             <Link href={link} className="flex items-center gap-2 text-sm mt-5">
-              Read more <Image src={assets.right_arrow} className="w-4" />
+              Read more
+              <Image src={assets.right_arrow} alt="" className="w-4" />
             </Link>
           </div>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
